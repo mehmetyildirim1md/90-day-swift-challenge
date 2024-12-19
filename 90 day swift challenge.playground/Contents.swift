ec0f13d02c7bd87e0599b0dd3a636f0bdbf7c926
mehmetyildirim1md <myildirim1md@gmail.com>
2024-12-19 15:31:06 +0300

    day 2 notes

--- 90 day swift challenge.playground/Contents.swift
+++ 90 day swift challenge.playground/Contents.swift
@@ -1,4 +1,36 @@
 import UIKit
 
-var greeting = "Hello, playground"
-print(greeting)
+//var greeting = "Hello, playground"
+//print(greeting)
+//değişkenler
+// 2 şekilde değişken tanımlanıyor: 1-var 2-let
+
+//var değişken kullanımı
+
+var isim = "Mehmet"
+isim
+
+//let değişken kullanımı
+
+let yas = 25
+
+// var değişkeninin içeriği değiştirilebilirken let için bu mümkün değildir.
+
+
+// let için pi sayısı, üçgenin içi açıları toplamı gibi değiştirmeyeceğimiz değerleri atayabiliriz.
+
+// değişken tipleri
+
+//string:metinsel değerler
+//integer:tam sayı değerleri
+//double: rasyonel değerler
+//boolen:doğru mu yanlış mı
+
+//var myAge :Int demek bu değişkeni integer olarak tanımladım demek
+// string ifadesini tek imleç yerien 3 imleçle yazarsan alt alta yazabilirsin
+var uzunAda = """
+ benim bir
+adam olsun.
+"""
+uzunAda
+       
